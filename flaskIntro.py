3c7646b5dd22cb7a36fdbe389c1fd1e2373787e5
Simon T. <simtho0624@gmail.com>
2025-04-08 11:20:31 +0200

    add more routes

--- flaskIntro.py
+++ flaskIntro.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
+
+@app.route('/contact')
+def contact():
+    return "<h1>Kontakta oss!!!</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1> This is me :D</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
